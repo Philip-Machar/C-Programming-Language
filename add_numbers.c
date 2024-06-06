8f8ed77238767cd19f5fc238c6f10f49ef9e6c6e
Philip-Machar <philipmachar788@gmail.com>
2024-06-06 15:15:51 +0300

    adding two numbers

---
+++ add_numbers.c
@@ -0,0 +1,21 @@
+//importing the standard input/output library
+#include <stdio.h>
+
+//This is the main C function where code execution begins 
+int main() {
+    //variables to contain number 1 and number 2
+    int num1 = 10;
+    int num2 = 20;
+
+    /*
+    adding number 1 to number 2 and storing the 
+    result in a variable called sum
+    */
+    int sum = num1 + num2;
+
+    //displaying the results to the terminal
+    printf("The sum of these two numbers is %d", sum);
+
+    //telling the OS that the code was successfully executed
+    return 0;
+}
